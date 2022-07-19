bd5b876dcd3cb582e22be6ea76fcc057d6d56c54
Rachel <rachelmorris262@outlook.com>
2022-07-19 10:48:41 +0100

    Initial commit

---
+++ initialCall.py
@@ -0,0 +1,10 @@
+import requests
+
+# An API request for the cocktail margarita
+url = 'http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
+
+response = requests.get(url)
+cocktail = response.json()
+
+# Prints the name of the cocktail
+print(cocktail['drinks'][0]['strDrink'])
