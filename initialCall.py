bcdf79cad799b707a65deed77ad613e4f7b76add
Rachel <rachelmorris262@outlook.com>
2022-07-19 12:25:23 +0100

    change code to show more details and use format strings

--- initialCall.py
+++ initialCall.py
@@ -6,6 +6,20 @@ url = 'http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
 response = requests.get(url)
 cocktail = response.json()
 
-# Prints the name of the cocktail
-print(cocktail['drinks'][0]['strDrink'])
-print(cocktail['drinks'][0]['strInstructions'])
+# Requests the details of the cocktail
+drinkDetails = cocktail['drinks'][0]
+
+name = drinkDetails['strDrink']
+alcoholic = drinkDetails['strAlcoholic']
+glass = drinkDetails['strGlass']
+instructions = drinkDetails['strInstructions']
+
+# Prints the details of the cocktails with explanations
+print(f"Name: {name} \n")
+if alcoholic == 'Alcoholic':
+    print("Is the drink alcoholic: Yes \n")
+else:
+    print("Is the drink alcoholic: No \n")
+print(f"Type of glass: {glass} \n")
+print(f"Instructions: {instructions}  ")
+
